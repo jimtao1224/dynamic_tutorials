44abaaa8990aa42dc7abd39a76910734aea3a746
table <table>
2022-10-06 16:01:18 +0800

    first

---
+++ test_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/jim/workspace/test_ws/src
+# Build directory: /home/jim/workspace/test_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("dynamic_tutorials")
